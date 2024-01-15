fb72b50958176f02b93ad51efc02a7edc51a40f7
Will Duquette <will@wjduquette.com>
2024-01-15 10:07:35 -0800

    Lint fixes.

--- app/src/main/java/pen/tools/demo/DemoTool.java
+++ app/src/main/java/pen/tools/demo/DemoTool.java
@@ -142,14 +142,14 @@ Java API.
         sten.clear(Color.WHITE);
 
         sten.translate(70,70);
-        sten.draw(sten.line().to( 15,  0).to( 50,   0).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
-        sten.draw(sten.line().to( 15,-15).to( 50, -50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
-        sten.draw(sten.line().to(  0,-15).to(  0, -50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
-        sten.draw(sten.line().to(-15,-15).to(-50, -50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
-        sten.draw(sten.line().to(-15,  0).to(-50,   0).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
-        sten.draw(sten.line().to(-15, 15).to(-50,  50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
-        sten.draw(sten.line().to(  0, 15).to(  0,  50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
-        sten.draw(sten.line().to( 15, 15).to( 50,  50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to( 15,  0).to( 50,   0).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to( 15,-15).to( 50, -50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to(  0,-15).to(  0, -50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to(-15,-15).to(-50, -50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to(-15,  0).to(-50,   0).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to(-15, 15).to(-50,  50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to(  0, 15).to(  0,  50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
+        sten.draw(Stencil.line().to( 15, 15).to( 50,  50).start(Symbol.ARROW_SOLID).end(Symbol.ARROW_OPEN));
     }
 
 
