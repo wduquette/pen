51f7d4534fedeb61dc653a75e015f877cabd88ae
Will Duquette <will@wjduquette.com>
2024-03-15 17:14:11 -0700

    Extended `Limit` to include an explicit `Cap` type.

---
+++ app/src/main/java/pen/diagram/timeline/Cap.java
@@ -0,0 +1,21 @@
+package pen.diagram.timeline;
+
+public enum Cap {
+    /**
+     * An entity bar cap drawn with a solid line. Used for entities with a
+     * definite end time.
+     */
+    HARD,
+
+    /**
+     * An entity bar cap drawn with an invisible line.  Used for entity
+     * bars that run past the end of the diagram.
+     */
+    SOFT,
+
+    /**
+     * A cap that extends past the limit in a fuzzy way.  Used for entities
+     * whose lifetime extends before or after the time interval of interest.
+     */
+    FUZZY
+}
--- app/src/main/java/pen/diagram/timeline/Limit.java
+++ app/src/main/java/pen/diagram/timeline/Limit.java
@@ -4,11 +4,15 @@ package pen.diagram.timeline;
 public sealed interface Limit
     permits Limit.Fixed, Limit.EventBased, Limit.External
 {
-    record Fixed(int day) implements Limit {}
-    record EventBased()   implements Limit {}
-    record External()     implements Limit {}
+    Cap cap();
 
-    static Limit fixed(int day) { return new Fixed(day); }
-    static Limit eventBased()   { return new EventBased(); }
-    static Limit external()     { return new External();  }
+    record Fixed(Cap cap, int day) implements Limit {}
+    record EventBased(Cap cap)   implements Limit {}
+    record External(Cap cap)     implements Limit {}
+
+    static Limit fixed(int day)          { return new Fixed(Cap.HARD, day); }
+    static Limit fixed(Cap cap, int day) { return new Fixed(cap, day); }
+    static Limit eventBased()            { return new EventBased(Cap.HARD); }
+    static Limit eventBased(Cap cap)     { return new EventBased(cap); }
+    static Limit external()              { return new External(Cap.SOFT);  }
 }
