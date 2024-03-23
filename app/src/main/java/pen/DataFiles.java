f2924d88805c01b5fe5cf6c8f01228ebaf5df058
Will Duquette <will@wjduquette.com>
2024-03-23 16:08:01 -0700

    Added calendar file loader.

--- app/src/main/java/pen/DataFiles.java
+++ app/src/main/java/pen/DataFiles.java
@@ -1,6 +1,8 @@
 package pen;
 
+import pen.apis.CalendarExtension;
 import pen.apis.HistoryExtension;
+import pen.calendars.Calendar;
 import pen.history.HistoryBank;
 import pen.tcl.TclEngine;
 import pen.tcl.TclEngineException;
@@ -8,6 +10,8 @@ import pen.tcl.TclEngineException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Static class for loading data formats.
@@ -15,6 +19,36 @@ import java.nio.file.Path;
 public class DataFiles {
     private DataFiles() {} // Not instantiable
 
+    /**
+     * Loads a calendar file, and returns a map containing the loaded
+     * calendars.
+     * @param path The path to the file
+     * @return The map
+     * @throws DataFileException on error
+     */
+    public static Map<String,Calendar> loadCalendar(Path path)
+        throws DataFileException
+    {
+        var engine = new TclEngine();
+        var calendarExtension = new CalendarExtension();
+        engine.install(calendarExtension);
+
+        try {
+            var script = Files.readString(path);
+            engine.eval(script);
+        } catch (Exception ex) {
+            throw error("history", ex);
+        }
+
+        return calendarExtension.getCalendars();
+    }
+
+    /**
+     * Loads a history file, and returns the loaded history.
+     * @param path The path to the file
+     * @return The history
+     * @throws DataFileException on error
+     */
     public static HistoryBank loadHistory(Path path)
         throws DataFileException
     {
@@ -24,15 +58,23 @@ public class DataFiles {
 
         try {
             var script = Files.readString(path);
-            engine.evalFile(path.toFile());
-        } catch (IOException ex) {
-            throw new DataFileException("Error reading history", ex);
-        } catch (TclEngineException ex) {
-            throw new DataFileException("Error in history", ex);
+            engine.eval(script);
         } catch (Exception ex) {
-            throw new DataFileException("Unexpected error while loading history", ex);
+            throw error("history", ex);
         }
 
         return historyExtension.getHistory();
+    }
+
+    private static DataFileException error(String what, Exception ex) {
+        return switch (ex) {
+            case IOException e ->
+                new DataFileException("Error reading " + what, ex);
+            case TclEngineException e ->
+                new DataFileException("Error in " + what, ex);
+            default ->
+                new DataFileException(
+                    "Unexpected error while loading " + what, ex);
+        };
     }
 }
