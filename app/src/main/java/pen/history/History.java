463c21dd3873d70fc7eb9c02b9f258dcb95c24f5
Will Duquette <will@wjduquette.com>
2024-03-23 09:47:30 -0700

    Lint fixes.

--- app/src/main/java/pen/history/History.java
+++ app/src/main/java/pen/history/History.java
@@ -3,7 +3,6 @@ package pen.history;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Predicate;
 
 /**
  * A History consists of incidents, each of which may concern some number
