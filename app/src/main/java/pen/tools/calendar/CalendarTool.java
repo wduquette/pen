b39a480f56483af7977091c6241f046ac449692e
Will Duquette <will@wjduquette.com>
2024-04-15 19:40:02 -0700

    Made the window slightly bigger by default.

--- app/src/main/java/pen/tools/calendar/CalendarTool.java
+++ app/src/main/java/pen/tools/calendar/CalendarTool.java
@@ -59,7 +59,7 @@ public class CalendarTool extends FXTool {
         main = new MainView(this);
 
         // NEXT, pop up the window
-        Scene scene = new Scene(main, 800, 600);
+        Scene scene = new Scene(main, 800, 700);
 
         stage.setTitle("pen calendar - " +
             dataPath.getFileName());
